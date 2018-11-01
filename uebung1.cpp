1eca3af988d526b381b7ab5631d5786207909b45
oskarddfa <oskar.pfeffer@gmail.com>
2018-11-01 13:03:07 +0100

    irgendwas geändert

--- uebung1.cpp
+++ uebung1.cpp
@@ -8,57 +8,57 @@ double function(double x, double y){
   return x * x * ( - (y * y));
 }
 
-double partialintegral(double hxmin, double hxmax, double hymin, double hymax, int nxmax, int nymax){
-  double integral = 0;
-
-
 double integrate(double hxmin, double hxmax, double hymin, double hymax, int nxmax, int nymax){
   double deltahx, deltahy;
   double integral = 0;
-  double time1 = 0.0, tstart;
-  double nstart;
+  double nystart;
   deltahx = (hxmax - hxmin) / nxmax;
   deltahy = (hymax - hymin) / nymax;
-  tstart = clock();
-  omp_get_num_threads()
-  nystart = omp_get_thread_num() * nymax / omp_get_num_threads()
+  omp_get_num_threads();
+  nystart = omp_get_thread_num() * nymax / omp_get_num_threads();
   for (int ny = nystart; ny < nymax; ny++) {
     double c = 0;
+    printf("nyyyy =%d\n", ny);
     for (int nx = 0; nx < nxmax; nx++) {
       c += deltahx * deltahy / 4 * (
         function(hxmin + deltahx * nx      , hymin + deltahy * ny      ) +
         function(hxmin + deltahx * (nx + 1), hymin + deltahy * ny      ) +
         function(hxmin + deltahx * nx      , hymin + deltahy * (ny + 1)) +
         function(hxmin + deltahx * (nx + 1), hymin + deltahy * (ny + 1)));
-      //printf("c = %f\n", c);
-      //printf("int = %f\n", integral);
-  return integral
+      printf("c = %f\n", c);
+    integral +=c;
+    printf("nystart = %f\n", omp_get_thread_num() * nymax / omp_get_num_threads());
+    //printf("int = %f\n", integral);
+
+  return integral;
 
     }
     //integral +=c;
   }
-  time1 += clock() - tstart;
-  time1 = time1/CLOCKS_PER_SEC;
-  printf("%f\n", time1);
+
   return integral;
 }
 int main(int argc, char *argv[]){
   double time1=0.0, tstart;
-  int nthreads, tid;
   double hxmin = 0., hymin = 0., hxmax = 1., hymax = 1.;
   int nxmax = 8000000, nymax = 8;
   double integral = 0;
-  #pragma omp parallel{
-    #pragma omp single{
-      for (int if = 0; if < count; if++) {
-        /* code */
-      }
-      #pragma omp task{
+  tstart = clock();
+  #pragma omp parallel
+  {
+    #pragma omp single
+    {
+
+      #pragma omp task
+      {
         integral +=integrate(hxmin, hxmax, hymin, hymax, nxmax, nymax);
+        printf("thread nr:%d\n", omp_get_thread_num());
       }
     }
   }
-  integral = integrate(hxmin, hxmax, hymin, hymax, nxmax, nymax);
+  time1 += clock() - tstart;
+  time1 = time1/CLOCKS_PER_SEC;
+  printf("%f\n", time1);
   printf("%f\n", integral);
 
 
@@ -67,20 +67,5 @@ int main(int argc, char *argv[]){
   time1 = time1/CLOCKS_PER_SEC;
   //printf("%lf\n",time1 );
 
-  #pragma omp parallel for
-  for (size_t i = 0; i < 1000; i++) {
-    //printf("I'm thread %i of %d\n", omp_get_thread_num(), omp_get_num_threads());
-  }
-  /* Fork a team of threads giving them their own copies of variables */
-  #pragma omp parallel private(nthreads, tid)
-    {
-    tid = omp_get_thread_num();
-    //printf("Hello World from thread = %d\n", tid);
 
-    if (tid == 0)
-      {
-      nthreads = omp_get_num_threads();
-      //printf("Number of threads = %d\n", nthreads);
-    }
-  }
 }
