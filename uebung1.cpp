0f6ed93f2013c7b6d3dbbb02d7361143e335c8ab
oskarddfa <oskar.pfeffer@gmail.com>
2018-11-01 11:35:38 +0100

    x*x

--- uebung1.cpp
+++ uebung1.cpp
@@ -5,21 +5,26 @@
 #include <math.h>
 
 double function(double x, double y){
-  return pow(x, 2.0) * pow(M_E, - pow(y, 2));
+  return x * x * ( - (y * y));
 }
+
+double partialintegral(double hxmin, double hxmax, double hymin, double hymax, int nxmax, int nymax){
+  double integral = 0;
+
 
 double integrate(double hxmin, double hxmax, double hymin, double hymax, int nxmax, int nymax){
   double deltahx, deltahy;
   double integral = 0;
-  double time1=0.0, tstart;
+  double time1 = 0.0, tstart;
+  double nstart;
   deltahx = (hxmax - hxmin) / nxmax;
   deltahy = (hymax - hymin) / nymax;
   tstart = clock();
-
-  #pragma omp parallel for
-  for (size_t nx = 0; nx < nxmax; nx++) {
+  omp_get_num_threads()
+  nystart = omp_get_thread_num() * nymax / omp_get_num_threads()
+  for (int ny = nystart; ny < nymax; ny++) {
     double c = 0;
-    for (size_t ny = 0; ny < nymax; ny++) {
+    for (int nx = 0; nx < nxmax; nx++) {
       c += deltahx * deltahy / 4 * (
         function(hxmin + deltahx * nx      , hymin + deltahy * ny      ) +
         function(hxmin + deltahx * (nx + 1), hymin + deltahy * ny      ) +
@@ -27,9 +32,10 @@ double integrate(double hxmin, double hxmax, double hymin, double hymax, int nxm
         function(hxmin + deltahx * (nx + 1), hymin + deltahy * (ny + 1)));
       //printf("c = %f\n", c);
       //printf("int = %f\n", integral);
+  return integral
 
     }
-    integral +=c;
+    //integral +=c;
   }
   time1 += clock() - tstart;
   time1 = time1/CLOCKS_PER_SEC;
@@ -40,8 +46,18 @@ int main(int argc, char *argv[]){
   double time1=0.0, tstart;
   int nthreads, tid;
   double hxmin = 0., hymin = 0., hxmax = 1., hymax = 1.;
-  int nxmax = 4000, nymax = 4000;
-  double integral;
+  int nxmax = 8000000, nymax = 8;
+  double integral = 0;
+  #pragma omp parallel{
+    #pragma omp single{
+      for (int if = 0; if < count; if++) {
+        /* code */
+      }
+      #pragma omp task{
+        integral +=integrate(hxmin, hxmax, hymin, hymax, nxmax, nymax);
+      }
+    }
+  }
   integral = integrate(hxmin, hxmax, hymin, hymax, nxmax, nymax);
   printf("%f\n", integral);
 
